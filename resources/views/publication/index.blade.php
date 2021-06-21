aa758e4255a10548f2b88758fa28031bca819a0c
camosqueraba <camosqueraba@gmail.com>
2021-06-20 23:05:20 -0500

    afinando index anuncio

--- resources/views/publication/index.blade.php
+++ resources/views/publication/index.blade.php
@@ -1,14 +1,14 @@
-@extends('layouts.app')    
+@extends('layouts.app')
 @section('content')
 <div class="container">
 <a class="btn btn-primary" href="{{url('publication/create')}}">Crear Nueva Publicacion</a>
 <br>
-<br>
+
 @if (Session::has('mensaje'))
     {{Session::get('mensaje')}}
 @endif
-<table class="table table-light">
-    <thead class="thead-light">  
+<table class="table table-sm table-light">
+    <thead class="thead-light">
         <tr>
             <th>Id</th>
             <th>Tipo</th>
@@ -27,39 +27,39 @@
     <tbody>
         @foreach ($datos as $publication)
         <tr>
-            <td class="celda-contenido">{{$publication->id}}</td>
+            <td>{{$publication->id}}</td>
             <td>{{$publication->tipo}}</td>
             <td>{{$publication->titulo}}</td>
             <td>{{$publication->descripcion}}</td>
-            <td>{{$publication->contenido}}</td>
-            <td>{{$publication->link}}</td>
+            <td><div class="celda-contenido">{{$publication->contenido}}</div></td>
+            <td><div class="celda-enlace">{{$publication->link}}</div></td>
             <td>
-                <img src="{{asset('storage').'/'.$publication->imagen}}" width="100" alt="{{$publication->imagen}}">    
+                <img src="{{asset('storage').'/'.$publication->imagen}}" width="100" alt="{{$publication->imagen}}">
             </td>
             <td>
                <!-- <iframe width="150" height="100" frameborder="0" allowfullscreen=""   src="{{$publication->video}}">-->
-               
+
             </td>
             <td>{{$publication->documento}}</td>
             <td>{{$publication->fecha_inicio}}</td>
             <td>{{$publication->fecha_fin}}</td>
-            <td>
-                <a class="btn btn-success" href="{{url('publication/'.$publication->id.'/edit')}}">
+            <td class="col-2">
+                <a class="btn btn-success d-inline" href="{{url('publication/'.$publication->id.'/edit')}}">
                     Editar
                 </a>
-                
+
                 <form  action="{{url('/publication/'.$publication->id)}}" class="d-inline" method="POST" >
                     @csrf
                     {{method_field('DELETE')}}
                     <input class="btn btn-danger d-inline" type="submit" onclick="return confirm('¿Quieres eliminar?')" value="Borrar">
                 </form>
             </td>
-        </tr>    
+        </tr>
         @endforeach
-        
+
     </tbody>
 </table>
 {!!$datos->links()!!}
 </div>
 
-@endsection    
+@endsection
